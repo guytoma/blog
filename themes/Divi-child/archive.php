b1265f71678c59c2683643cac4451b5e239dae20
Guy Toma <info@kazeko.nl>
2017-08-01 23:29:27 +0200

    Fixes to CSS and index and added archive.php

--- themes/Divi-child/archive.php
+++ themes/Divi-child/archive.php
@@ -4,25 +4,7 @@
 	<div class="container">
 		<div id="content-area" class="clearfix">
 			<div id="left-area">
-<!-- Begin uitgelichte posts -->
-<div id="uitgelicht">
-	<div id="hoofdpost">
-		<?php query_posts('posts_per_page=1&cat=2553'); /*1, 2*/
-		if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-		<a href="<?php the_permalink(); ?>">
-			<img src="<?php the_post_thumbnail_url(); ?> ">
-			<div class="content">
-				<h2><?php the_title();/*3*/ ?></h2>
-				<p><?php the_excerpt(); ?></p>
-		</a>
-			<div class="postmeta">
-				<img src="<?php echo get_avatar_url( get_the_author_meta('ID'), 60 ) ?>">
-				<p><?php the_author(); ?><br><?php the_time('j F, Y') ?></p>
-			</div>
-		</div>
-		<?php endwhile; ?> <?php wp_reset_query(); /*4*/ ?>
-	</div>
-</div><!-- eind uitgelichte post -->
+
 <!-- begin laatste posts -->
 <div id="laatsteposts">
 	<?php rewind_posts(); ?>
